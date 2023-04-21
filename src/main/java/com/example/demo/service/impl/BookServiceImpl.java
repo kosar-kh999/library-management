38768e653612c088b696e59b92e517e8050b791d
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-04-21 23:45:09 +0430

    Book service logic

---
+++ src/main/java/com/example/demo/service/impl/BookServiceImpl.java
@@ -0,0 +1,31 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.data.model.Book;
+import com.example.demo.data.repository.BookRepository;
+import com.example.demo.service.BookService;
+import com.example.demo.util.exception.NotFoundBook;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(Book book) {
+        bookRepository.save(book);
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElseThrow(() -> new NotFoundBook("This book is not found"));
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+}
