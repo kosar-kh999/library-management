1241679e282d5a968f17ab14e7fa998af82b1ec2
kosar khodadadi <kosar.khodadadi999@gmail.com>
2023-04-21 23:50:05 +0430

    PersonSignUpDto for controller

---
+++ src/main/java/com/example/demo/data/dto/PersonSignUpDto.java
@@ -0,0 +1,24 @@
+package com.example.demo.data.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Pattern;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PersonSignUpDto {
+    @Pattern(regexp = "^[A-Za-z]\\w{2,29}$", message = "the name must be more than 2 character")
+    private String username;
+
+    @Email(message = "The format of email must be correct")
+    private String email;
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z]).{8}$")
+    private String password;
+}
